2558481ae5c7198d5aee79997ad2b5c3cb8ededb
pourya pooryeganeh <pour.pourya1999@gmail.com>
2022-04-15 16:49:20 +0430

    'softmax'

--- data_transform.py
+++ data_transform.py
@@ -33,7 +33,24 @@ class ToTensorClass:
         inp, out = sample
         return torch.from_numpy(inp), torch.from_numpy(out)
 
+class MulTransformClass:
+
+    def __init__(self, factor):
+        self.factor = factor
+
+    def __call__(self, sample):
+        inp, out = sample
+        inp *= self.factor
+        return inp, out
+
 dataset = WineDataset(transform = ToTensorClass())
+first_data = dataset[0]
+features, label = first_data
+print(features, label)
+print(type(features), type(label))
+
+composed = torchvision.transforms.Compose([ToTensorClass(), MulTransformClass(2)])
+dataset = WineDataset(transform = composed)
 first_data = dataset[0]
 features, label = first_data
 print(features, label)
