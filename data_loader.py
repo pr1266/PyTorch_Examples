f8ed7cf20c5c5111e73e11dfe974e147ba842b23
pourya pooryeganeh <pour.pourya1999@gmail.com>
2022-04-15 16:20:56 +0430

    'dataloader'

--- data_loader.py
+++ data_loader.py
@@ -15,9 +15,17 @@ class WineDataset(Dataset):
         self.x = torch.from_numpy(xy[:, 1:])
         self.y = torch.from_numpy(xy[:, [0]])
         self.n_samples = xy.shape[0]
-        
+
     def __getitem__(self, index):
         return self.x[index], self.y[index]
 
     def __len__(self):
         return self.n_samples
+
+dataset = WineDataset()
+dataloader = DataLoader(dataset = dataset, batch_size = 4, shuffle = True)
+
+dataiter = iter(dataloader)
+data = dataiter.next()
+features, label = data
+print(features, label)
