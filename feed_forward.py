dafb5be96751f5ad417e0cce7a955f177b996594
pourya pooryeganeh <pour.pourya1999@gmail.com>
2022-04-24 20:11:10 +0430

    'complete_feed_forward'

--- feed_forward.py
+++ feed_forward.py
@@ -1,79 +1,103 @@
-from sklearn.utils import shuffle
 import torch
 import torch.nn as nn
 import torchvision
 import torchvision.transforms as transforms
 import matplotlib.pyplot as plt
 
-#! device:
+# Device configuration
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 print(device)
-
-#! hyper parameters:
-input_size = 784 #28*28
-hidden_size = 100
+# Hyper-parameters 
+input_size = 784 # 28x28
+hidden_size = 500 
 num_classes = 10
-num_epochs = 10
-batch_size = 64
+num_epochs = 50
+batch_size = 100
 learning_rate = 0.001
 
-#! mnist
-train_dataset = torchvision.datasets.MNIST(root = './data', train = True, transform = transforms.ToTensor(), download = True)
-test_dataset = torchvision.datasets.MNIST(root = './data', train = False, transform = transforms.ToTensor())
+# MNIST dataset 
+train_dataset = torchvision.datasets.MNIST(root='./data', 
+                                           train=True, 
+                                           transform=transforms.ToTensor(),  
+                                           download=True)
 
+test_dataset = torchvision.datasets.MNIST(root='./data', 
+                                          train=False, 
+                                          transform=transforms.ToTensor())
 
-train_loader = torch.utils.data.DataLoader(dataset = train_dataset, batch_size = batch_size, shuffle = True)
-train_loader = torch.utils.data.DataLoader(dataset = test_dataset, batch_size = batch_size, shuffle = False)
+# Data loader
+train_loader = torch.utils.data.DataLoader(dataset=train_dataset, 
+                                           batch_size=batch_size, 
+                                           shuffle=True)
 
-example = iter(train_loader)
-samples, labels = example.next()
-print(samples.shape)
-print(labels.shape)
+test_loader = torch.utils.data.DataLoader(dataset=test_dataset, 
+                                          batch_size=batch_size, 
+                                          shuffle=False)
 
+examples = iter(test_loader)
+example_data, example_targets = examples.next()
 
 for i in range(6):
-    plt.subplot(2, 3, i+1)
-    plt.imshow(samples[i][0], cmap = 'gray')
-
+    plt.subplot(2,3,i+1)
+    plt.imshow(example_data[i][0], cmap='gray')
 # plt.show()
 
-class NeuralNet(nn.module):
-
+# Fully connected neural network with one hidden layer
+class NeuralNet(nn.Module):
     def __init__(self, input_size, hidden_size, num_classes):
         super(NeuralNet, self).__init__()
-        self.l1 = nn.Linear(input_size, hidden_size)
+        self.input_size = input_size
+        self.l1 = nn.Linear(input_size, hidden_size) 
         self.relu = nn.ReLU()
-        self.l2 = nn.Linear(hidden_size, num_classes)
-
+        self.l2 = nn.Linear(hidden_size, num_classes)  
+    
     def forward(self, x):
-
         out = self.l1(x)
         out = self.relu(out)
         out = self.l2(out)
+        # no activation and no softmax at the end
         return out
 
-model = NeuralNet(input_size, hidden_size, num_classes)
+model = NeuralNet(input_size, hidden_size, num_classes).to(device)
 
-#! Loss:
+# Loss and optimizer
 criterion = nn.CrossEntropyLoss()
-optimizer = torch.optim.Adam(parameters = model.parameters(), lr = learning_rate)
+optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)  
 
-#! train_loops:
+# Train the model
 n_total_steps = len(train_loader)
 for epoch in range(num_epochs):
-    for i, (images, labels) in enumerate(train_loader):
+    for i, (images, labels) in enumerate(train_loader):  
+        # origin shape: [100, 1, 28, 28]
+        # resized: [100, 784]
         images = images.reshape(-1, 28*28).to(device)
         labels = labels.to(device)
-
-        #! forward pass:
+        
+        # Forward pass
         outputs = model(images)
         loss = criterion(outputs, labels)
-
-        #! backward pass:
+        
+        # Backward and optimize
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
-
+        
         if (i+1) % 100 == 0:
-            print(f'current epoch : {epoch+1}/{num_epochs}, step : {i+1}/{n_total_steps}, loss : {loss.item():.4f}')
+            print (f'Epoch [{epoch+1}/{num_epochs}], Step [{i+1}/{n_total_steps}], Loss: {loss.item():.4f}')
 
+# Test the model
+# In test phase, we don't need to compute gradients (for memory efficiency)
+with torch.no_grad():
+    n_correct = 0
+    n_samples = 0
+    for images, labels in test_loader:
+        images = images.reshape(-1, 28*28).to(device)
+        labels = labels.to(device)
+        outputs = model(images)
+        # max returns (value ,index)
+        _, predicted = torch.max(outputs.data, 1)
+        n_samples += labels.size(0)
+        n_correct += (predicted == labels).sum().item()
+
+    acc = 100.0 * n_correct / n_samples
+    print(f'Accuracy of the network on the 10000 test images: {acc} %')
