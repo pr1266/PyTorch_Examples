ce4677aa8555659a15737be076a2611a2a9bea34
pourya pooryeganeh <pour.pourya1999@gmail.com>
2022-04-24 19:50:15 +0430

    'mnist'

--- feed_forward.py
+++ feed_forward.py
@@ -1,3 +1,4 @@
+from sklearn.utils import shuffle
 import torch
 import torch.nn as nn
 import torchvision
@@ -15,3 +16,23 @@ num_classes = 10
 num_epochs = 10
 batch_size = 64
 learning_rate = 0.001
+
+#! mnist
+train_dataset = torchvision.datasets.MNIST(root = './data', train = True, transform = transforms.ToTensor(), download = True)
+test_dataset = torchvision.datasets.MNIST(root = './data', train = False, transform = transforms.ToTensor())
+
+
+train_loader = torch.utils.data.DataLoader(dataset = train_dataset, batch_size = batch_size, shuffle = True)
+train_loader = torch.utils.data.DataLoader(dataset = test_dataset, batch_size = batch_size, shuffle = False)
+
+example = iter(train_loader)
+samples, labels = example.next()
+print(samples.shape)
+print(labels.shape)
+
+
+for i in range(6):
+    plt.subplot(2, 3, i+1)
+    plt.imshow(samples[i][0], cmap = 'gray')
+
+plt.show()
