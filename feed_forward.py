a09c27c56593c0988f01e23a7563a7438f03c392
pourya pooryeganeh <pour.pourya1999@gmail.com>
2022-04-24 20:01:06 +0430

    'train_phase'

--- feed_forward.py
+++ feed_forward.py
@@ -35,4 +35,45 @@ for i in range(6):
     plt.subplot(2, 3, i+1)
     plt.imshow(samples[i][0], cmap = 'gray')
 
-plt.show()
+# plt.show()
+
+class NeuralNet(nn.module):
+
+    def __init__(self, input_size, hidden_size, num_classes):
+        super(NeuralNet, self).__init__()
+        self.l1 = nn.Linear(input_size, hidden_size)
+        self.relu = nn.ReLU()
+        self.l2 = nn.Linear(hidden_size, num_classes)
+
+    def forward(self, x):
+
+        out = self.l1(x)
+        out = self.relu(out)
+        out = self.l2(out)
+        return out
+
+model = NeuralNet(input_size, hidden_size, num_classes)
+
+#! Loss:
+criterion = nn.CrossEntropyLoss()
+optimizer = torch.optim.Adam(parameters = model.parameters(), lr = learning_rate)
+
+#! train_loops:
+n_total_steps = len(train_loader)
+for epoch in range(num_epochs):
+    for i, (images, labels) in enumerate(train_loader):
+        images = images.reshape(-1, 28*28).to(device)
+        labels = labels.to(device)
+
+        #! forward pass:
+        outputs = model(images)
+        loss = criterion(outputs, labels)
+
+        #! backward pass:
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        if (i+1) % 100 == 0:
+            print(f'current epoch : {epoch+1}/{num_epochs}, step : {i+1}/{n_total_steps}, loss : {loss.item():.4f}')
+
